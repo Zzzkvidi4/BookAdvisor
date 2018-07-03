ecb73a96b05646fd8e8722cef60ebf7cb3360a0d
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-07-03 19:56:32 +0300

    Created Book model

---
+++ src/main/java/com/zzzkvidi4/bookadvisor/model/Book.java
@@ -0,0 +1,47 @@
+package com.zzzkvidi4.bookadvisor.model;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Book {
+    public enum Resources {OZON, CHITAI_GOROD, LITRES}
+    private String author;
+    private String title;
+    private BigDecimal price;
+    private List<Pair<Resources, String>> ids = new LinkedList<>();
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public List<Pair<Resources, String>> getIds() {
+        return ids;
+    }
+
+    public void addId(Resources resource, String id){
+        ids.add(Pair.of(resource, id));
+    }
+}
