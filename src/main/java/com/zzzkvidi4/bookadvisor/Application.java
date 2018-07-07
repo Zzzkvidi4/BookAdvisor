59eb62a76758f567f123665b9e071a7804304bf6
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-07-07 17:47:51 +0300

    Added cors configuration to create requests from angular server

--- src/main/java/com/zzzkvidi4/bookadvisor/Application.java
+++ src/main/java/com/zzzkvidi4/bookadvisor/Application.java
@@ -2,8 +2,16 @@ package com.zzzkvidi4.bookadvisor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Configuration
 @EnableAutoConfiguration
@@ -11,5 +19,22 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
     public static void main(String... args){
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        Logger.getLogger(getClass().getName()).info("Applying bean to configure corsFilter");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
