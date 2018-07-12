a1f61634dce5e0df8f2d882bfd7f5dc4f30f9821
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-07-12 21:23:24 +0300

    Implemented userDetailsService to use spring security Added it to security configuration

--- src/main/java/com/zzzkvidi4/bookadvisor/security/CustomUserDetailsService.java
+++ src/main/java/com/zzzkvidi4/bookadvisor/security/CustomUserDetailsService.java
@@ -1,12 +1,29 @@
 package com.zzzkvidi4.bookadvisor.security;
 
+import com.zzzkvidi4.bookadvisor.dbservice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        com.zzzkvidi4.bookadvisor.model.db.User user = userService.getUserByLogin(s);
+        User.UserBuilder builder = User.builder();
+        builder.username(user.getUsername());
+        builder.password(user.getPassword());
+        builder.roles("USER");
+        return builder.build();
     }
 }
--- src/main/java/com/zzzkvidi4/bookadvisor/security/SecurityConfiguration.java
+++ src/main/java/com/zzzkvidi4/bookadvisor/security/SecurityConfiguration.java
@@ -1,15 +1,29 @@
 package com.zzzkvidi4.bookadvisor.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private CustomUserDetailsService userDetailsService;
 
+    @Autowired
+    public void setUserDetailsService(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
