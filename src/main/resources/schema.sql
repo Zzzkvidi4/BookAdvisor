e452b26c21621741a47f2ba1cc579cbe2dc65578
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-07-12 21:19:30 +0300

    Fixed resources to not use auto creation of db in hibernate. Changed indexes to unique

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users(
   user_id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(20) NOT NULL UNIQUE,
-  password VARCHAR(20) NOT NULL
+  password VARCHAR(60) NOT NULL
 );
-CREATE INDEX username_index ON users(username);
+CREATE UNIQUE INDEX username_index ON users(username);
 
 CREATE TABLE books(
   book_id INT AUTO_INCREMENT PRIMARY KEY,
   selector VARCHAR(60) NOT NULL UNIQUE
 );
-CREATE INDEX selector_index ON books(selector);
+CREATE UNIQUE INDEX selector_index ON books(selector);
 
 CREATE TABLE users_books(
   book_id INT NOT NULL,
