35b7224f315fb804ceca30019bdf587a755e6db3
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-07-16 11:07:27 +0300

    Added new constructor for SearchQuery class

--- src/main/java/com/zzzkvidi4/bookadvisor/model/SearchQuery.java
+++ src/main/java/com/zzzkvidi4/bookadvisor/model/SearchQuery.java
@@ -1,11 +1,19 @@
 package com.zzzkvidi4.bookadvisor.model;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SearchQuery {
     private String selector;
     private Set<Book.Resource> resources = new HashSet<>();
+
+    public SearchQuery(){}
+
+    public SearchQuery(String selector, Book.Resource... resources){
+        this.selector = selector;
+        this.resources.addAll(Arrays.asList(resources));
+    }
 
     public String getSelector() {
         return selector;
