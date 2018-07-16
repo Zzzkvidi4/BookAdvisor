45a2c6d054bd606a87033cd8da8e067f98ff0b0d
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-07-16 08:07:37 +0300

    Added adding to favourite and check whether it is already in favourite or not

--- src/main/book-advisor-web/src/app/service/user-service/user.service.ts
+++ src/main/book-advisor-web/src/app/service/user-service/user.service.ts
@@ -3,6 +3,8 @@ import {HttpClient, HttpResponse} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 import {User} from "../../model/User";
 import {ResponseContainer} from "../../model/ResponseContainer";
+import {Book} from "../../model/Book";
+import {Review} from "../../model/Review";
 
 @Injectable()
 export class UserService {
@@ -11,5 +13,17 @@ export class UserService {
 
   getUser(id: number): Observable<HttpResponse<ResponseContainer<User>>> {
     return this.http.get<ResponseContainer<User>>("http://localhost:8080/users/"+id, {observe: "response", withCredentials: true});
+  }
+
+  addToFavourite(id: number, book: Book): Observable<HttpResponse<ResponseContainer<boolean>>> {
+    return this.http.post<any>("http://localhost:8080/users/" + id + "/favourites", book, {observe: "response", withCredentials: true});
+  }
+
+  isInFavourite(id: number, book: Book): Observable<HttpResponse<ResponseContainer<boolean>>> {
+    return this.http.post<ResponseContainer<boolean>>("http://localhost:8080/users/" + id + "/favourites/is-in", book, {observe: "response", withCredentials: true});
+  }
+
+  getReviewsFavourite(userId: number, bookId: number): Observable<HttpResponse<ResponseContainer<Review[]>>> {
+    return this.http.get<ResponseContainer<Review[]>>("http://localhost:8080/users/" + userId + "/favourites/" + bookId, {observe: "response", withCredentials: true});
   }
 }
