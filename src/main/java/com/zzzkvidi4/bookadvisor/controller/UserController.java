777e2e03f0fef97fcacc2245b9b53438f60eccd6
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-07-16 11:03:59 +0300

    Added retrieving of reviews from favourite book

--- src/main/java/com/zzzkvidi4/bookadvisor/controller/UserController.java
+++ src/main/java/com/zzzkvidi4/bookadvisor/controller/UserController.java
@@ -2,22 +2,40 @@ package com.zzzkvidi4.bookadvisor.controller;
 
 import com.zzzkvidi4.bookadvisor.dbservice.UserService;
 import com.zzzkvidi4.bookadvisor.model.Book;
+import com.zzzkvidi4.bookadvisor.model.Review;
+import com.zzzkvidi4.bookadvisor.model.SearchQuery;
 import com.zzzkvidi4.bookadvisor.model.db.User;
 import com.zzzkvidi4.bookadvisor.response.ResponseContainer;
+import com.zzzkvidi4.bookadvisor.searcher.BookSearcherService;
+import com.zzzkvidi4.bookadvisor.searcher.ReviewRetrieverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
 public class UserController {
     private UserService userService;
+    private ReviewRetrieverService reviewRetrieverService;
+    private BookSearcherService bookSearcherService;
 
     @Autowired
     public void setUserService(UserService userService){
         this.userService = userService;
+    }
+
+    @Autowired
+    public void setReviewRetrieverService(ReviewRetrieverService reviewRetrieverService) {
+        this.reviewRetrieverService = reviewRetrieverService;
+    }
+
+    @Autowired
+    public void setBookSearcherService(BookSearcherService bookSearcherService) {
+        this.bookSearcherService = bookSearcherService;
     }
 
     @RequestMapping(path = "/users", method = RequestMethod.GET)
@@ -54,7 +72,7 @@ public class UserController {
     }
 
     @RequestMapping(path = "/users/{id}/favourites", method = RequestMethod.POST)
-    public ResponseContainer<Boolean> addToFavourite(@PathVariable int id, @RequestBody Book book){
+    public ResponseContainer<Boolean> addToFavourite(@PathVariable int id, @RequestBody com.zzzkvidi4.bookadvisor.model.db.Book book){
         ResponseContainer<Boolean> response = new ResponseContainer<>();
         response.setAuthenticated(true);
         response.setData(isCurrentUser(id));
@@ -72,6 +90,24 @@ public class UserController {
         if (response.getData()) {
             response.setData(userService.isInFavourite(id, book));
         }
+        return response;
+    }
+
+    @RequestMapping(path = "/users/{userId}/favourites/{bookId}", method = RequestMethod.GET)
+    public ResponseContainer<Collection<Review>> getReviewsFromFavourite(@PathVariable(name = "userId") int userId, @PathVariable(name = "bookId") int bookId){
+        com.zzzkvidi4.bookadvisor.model.db.Book dbBook = userService.getBookFromFavourite(userId, bookId);
+        ResponseContainer<Collection<Review>> response = new ResponseContainer<>();
+        response.setAuthenticated(true);
+        if ((dbBook == null) || (!isCurrentUser(userId))) {
+            return response;
+        }
+        Collection<Book> books = bookSearcherService.getBooks(new SearchQuery(dbBook.getSelector(), Book.Resource.LITRES, Book.Resource.OZON));
+        books.removeIf(book -> !dbBook.getAuthor().equals(book.getAuthor().toLowerCase()) || !dbBook.getTitle().equals(book.getTitle().toLowerCase()));
+        List<Review> reviews = new LinkedList<>();
+        for(Book book: books){
+            reviews.addAll(reviewRetrieverService.getReviews(book));
+        }
+        response.setData(reviews);
         return response;
     }
 
