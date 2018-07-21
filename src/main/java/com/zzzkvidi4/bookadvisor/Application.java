46267ceded04e90b3900f0f0ff53ffbdc959fde7
Zzzkvidi4 <zzzkvidi4@gmail.com>
2018-07-21 18:49:47 +0300

    Enabled aspect system and created annotation for logging purposes

--- src/main/java/com/zzzkvidi4/bookadvisor/Application.java
+++ src/main/java/com/zzzkvidi4/bookadvisor/Application.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,6 +25,7 @@ import java.util.logging.Logger;
 @Configuration
 @EnableAutoConfiguration
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 @ComponentScan
 public class Application {
     private static Logger logger = Logger.getLogger(Application.class.getName());
